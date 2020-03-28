6d16a189756f67fd015125516772138e15cbba4c
Wildes Piva <wildeshenrique8@gmail.com>
2020-03-28 16:35:25 -0300

    Back-end da applicação

---
+++ backend/knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+// Efetuar migrations: npx knex migrate:latest
+// Voltar ultima migration: npx knex migrate:rollback
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/database/db.sqlite'
+    },
+    migrations: {
+      directory: './src/database/migrations'
+    },
+    useNullAsDefault: true,
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
